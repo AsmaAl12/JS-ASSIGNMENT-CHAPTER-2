fbdc08bf5ac5eccac9267706252f25863dcd445c
Asma Ali <128274795+AsmaAl12@users.noreply.github.com>
2023-05-09 02:57:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,53 @@
+//======================Assignment # 2============================//
+//=========================VARIABLES=======================//
+//Question1. 
+    //var username = "AsmaAli";
+
+
+//Question2. 
+    //var myName = "AsmaAli";
+
+
+//Question3. 
+    //var message = "Hello World"
+    //alert (message);
+
+
+//Question4.
+    //var studentName = "Jhone Doe";
+    //alert (studentName);
+
+    //var studentAge = "15 years old";
+    //alert (studentAge);
+
+    //var studentSkill = "Certified Mobile Application Development";
+    //alert (studentSkill);
+
+
+//Question5.
+    //var name = " \n PIZZA \n PIZZ \n PIZ \n PI \n P";
+    //alert (name);
+
+
+//Question6. 
+    //var email = "example@example.com";
+    //alert ( "My email address is " + email);
+        
+
+//Question7. 
+    //var book = "A smarter way to learn JavaScript";
+    //alert ("I am trying to learn from the book " + book);
+
+
+//Question8.
+    //document.write("Yah I can write HTML content through JavaScript")
+
+
+//Question9.
+var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (string);
+
+
+
+
+//===================================================Chapter # 2 completed==========================================//
